985afb51b2450fa81a6ed322ed84605f855dd4e8
maddiehealy <maddiehealy@gmail.com>
2017-08-08 13:00:29 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ exports.handler = function(event, context, callback){
         stateHandlers.playModeIntentHandlers,
         stateHandlers.remoteControllerHandlers,
         stateHandlers.resumeDecisionModeIntentHandlers,
+        stateHandlers.whyCongress,
         audioEventHandlers
     );
 
